8abe404cdadc81356f09eec4d753b735e4f9bb46
MFANRUM <scherryrum@163.com>
2025-04-02 11:41:05 +0800

    Initial commit

---
+++ data-generator-core-service/src/main/java/com/mf/core/SpringGeneratorApplication.java
@@ -0,0 +1,9 @@
+package com.mf.core;
+
+/**
+ * @Description: 数据生成服务启动类
+ * @Auther: MFAN
+ * @Date: 2025/4/1 17:01:31
+ * @Version: 1.0
+ */
+public class SpringGeneratorApplication {}
---
+++ data-sender-service/src/main/java/com/mf/sender/SpringSenderApplication.java
@@ -0,0 +1,13 @@
+package com.mf.sender;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @Auther: MFAN
+ * @Date: 2025/4/1 17:53:15
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+public class SpringSenderApplication {}
